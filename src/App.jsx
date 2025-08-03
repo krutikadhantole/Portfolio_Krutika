8d46d9eba1668e46d392e23d619c273e12c6bbaa
krutika Dhantole <dhantolekrutika66@gmail.com>
2025-08-03 16:27:51 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import MyWork from "./components/MyWork/MyWork";
-import Contact from "./components/contact/contact";
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
